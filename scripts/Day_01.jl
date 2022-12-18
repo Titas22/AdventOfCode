748b1445efb61d1c96a1d44f1d68b18f450ee898
Titas22 <titas.bucelis@gmail.com>
2022-12-18 01:06:40 +0000

    add day 1 part 2 solution

--- scripts/Day_01.jl
+++ scripts/Day_01.jl
@@ -1,24 +1,29 @@
 # include("./scripts/Day_01.jl")
 
+function getSortedCalories(lines)
+    calories = Vector{Int64}()
 
-function getMaxSum(lines)
     currentCount = 0;
-    maxCount = 0;
-
     for l = lines
         if !isempty(l)
             currentCount += parse(Int64, l);
             continue
         end
-        if currentCount > maxCount
-            maxCount = currentCount;
-        end
+
+        push!(calories, currentCount)
         currentCount = 0;
     end
-    display("")
-    return maxCount;
+
+    sort!(calories, rev=true)
+
+    return calories;
 end
 
-lines = open("./inputs/2022/inputs_2022_01.txt") do file
-    display(getMaxSum(readlines(file)))
+lines = open("./inputs/2022/in_2022--01.txt") do file
+    lines = readlines(file);
 end
+
+@time calories = getSortedCalories(lines);
+
+println("\nPart 1 answer: $(calories[1])");
+println("\nPart 2 answer: $(sum(calories[1:3]))");
