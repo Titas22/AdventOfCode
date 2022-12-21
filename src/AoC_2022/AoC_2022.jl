1dbd7b3a5e172eef21affc273afc58727c64103d
Titas22 <titas.bucelis@gmail.com>
2022-12-21 01:39:39 +0000

    add day 1 benchmark

--- src/AoC_2022/AoC_2022.jl
+++ src/AoC_2022/AoC_2022.jl
@@ -3,4 +3,9 @@ module AoC_2022
 
     using AdventOfCode
     
+
+    # TODO: this doesn't work very nicely
+    include("./AoC_22_01.jl")
+    # import  ..AoC_22_01;
+    display(@benchmark (part1, part2) = AoC_22_01.solve());
 end 
