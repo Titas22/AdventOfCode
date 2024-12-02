9661651a660f6debe9b641d6e979dbaa22872cec
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-02 20:29:14 +0000

    refactor: clean up day 2 solution

--- src/AoC_2024/AoC_2024_02.jl
+++ src/AoC_2024/AoC_2024_02.jl
@@ -3,66 +3,40 @@ module AoC_2024_02
     const AoC = AdventOfCode;
 
     function parse_inputs(lines::Vector{String})
-
-        return lines;
-    end
-    function solve_common(inputs)
-
-        return inputs;
+        return (x->parse.(Int, x)).(split.(lines));
     end
 
-    function solve_part_1(inputs)
-
-        return nothing;
+    function is_safe_report(v::Vector{Int64}, recurse::Bool = false)
+        dv = diff(v)
+        if dv[1] > 0
+            b = dv .> 0 .&& dv .<= 3
+        else
+            b = dv .< 0 .&& dv .>= -3
+        end
+    
+        ~all(b) || return true
+        recurse || return false
+        for ii in eachindex(v)
+            ~is_safe_report(v[1:end .!= ii], false) || return true
+        end
+        return false
     end
 
-    function solve_part_2(inputs)
-
-        return nothing;
-    end
+    solve_part_1(inputs) = count(is_safe_report.(inputs));
+    solve_part_2(inputs) = count(is_safe_report.(inputs, true));
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
         lines       = @getinputs(btest);
-        # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
         inputs      = parse_inputs(lines);
 
-        solution    = solve_common(inputs);
-        part1       = solve_part_1(solution);
-        part2       = solve_part_2(solution);
+        part1       = solve_part_1(inputs);
+        part2       = solve_part_2(inputs);
 
         return (part1, part2);
     end
 
-    @time (part1, part2) = solve(true); # Test
+    @time (part1, part2) = solve(false); # Test
     # @time (part1, part2) = solve();
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(true)
-
-inputs = (x->parse.(Int, x)).(split.(lines))
-
-function is_safe_report(v::Vector{Int64}, recurse::Bool = false)
-    dv = diff(v)
-    if dv[1] > 0
-        b = dv .> 0 .&& dv .<= 3
-    else
-        b = dv .< 0 .&& dv .>= -3
-    end
-
-    ~all(b) || return true
-    recurse || return false
-    for ii in eachindex(v)
-        ~is_safe_report(v[1:end .!= ii], false) || return true
-    end
-    println(b)
-
-
-
-    return false
-end
-b = is_safe_report.(inputs)
-count(b)
-
-b = is_safe_report.(inputs, true)
-count(b)
