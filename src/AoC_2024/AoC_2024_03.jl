066781f9b4a086e71fed9109d0eb07765ba8e17d
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-03 21:55:10 +0000

    refactor: clean up day 3 solution

--- src/AoC_2024/AoC_2024_03.jl
+++ src/AoC_2024/AoC_2024_03.jl
@@ -1,24 +1,37 @@
 module AoC_2024_03
     using AdventOfCode;
-    const AoC = AdventOfCode;
 
-    function parse_inputs(lines::Vector{String})
-
-        return lines;
+    function parse_inputs(lines::Vector{String})::String 
+        return join(lines);
     end
-    function solve_common(inputs)
-
-        return inputs;
+    
+    function find_muls(input)
+        pattern = r"mul\((\d+),(\d+)\)";
+        a = [ parse.(Int, split(m.match[5:end-1], ',')) for m in eachmatch(pattern, input)]
+        return a;
     end
 
     function solve_part_1(inputs)
 
-        return nothing;
+        a = find_muls(inputs)
+        p1 = sum([x[1] * x[2] for x in a])
+        return p1;
     end
 
     function solve_part_2(inputs)
 
-        return nothing;
+        do_blocks = split(inputs, "do()")
+
+        mul_vals = Vector{Int}[]
+        for do_block in do_blocks
+            a1 = split(do_block, "don't()")[1]
+            b1 = find_muls(a1)
+            push!(mul_vals, b1...)
+        end
+        
+        p2 = sum([x[1] * x[2] for x in mul_vals])
+
+        return p2;
     end
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
@@ -26,44 +39,14 @@ module AoC_2024_03
         # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
         inputs      = parse_inputs(lines);
 
-        solution    = solve_common(inputs);
-        part1       = solve_part_1(solution);
-        part2       = solve_part_2(solution);
+        part1       = solve_part_1(inputs);
+        part2       = solve_part_2(inputs);
 
         return (part1, part2);
     end
 
-    @time (part1, part2) = solve(true); # Test
-    # @time (part1, part2) = solve();
+    # @time (part1, part2) = solve(true); # Test
+    @time (part1, part2) = solve();
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(true)
-
-input  = join(lines);
-# m = findall(r"mul\((\d+),(\d+)\)", lines[1])
-# # m = eachmatch(, lines[1])
-
-a = [ parse.(Int, split(m.match[5:end-1], ',')) for m in eachmatch(r"mul\((\d+),(\d+)\)", input)]
-
-p1 = sum([x[1] * x[2] for x in a])
-# parse.(Int, split(b[5:end-1], ','))
-
-do_blocks = split(input, "do()")
-
-mul_vals = Vector{Int}[]
-for do_block in do_blocks
-    a1 = split(do_block, "don't()")[1]
-    b1 = [ parse.(Int, split(m.match[5:end-1], ',')) for m in eachmatch(r"mul\((\d+),(\d+)\)", a1)]
-    push!(mul_vals, b1...)
-end
-
-# do_block = do_blocks[1]
-
-# x = split(do_block, "don't()")[1]
-
-mul_vals
-
-# a = split(do_block, "don't()")[1]
-# b = [ parse.(Int, split(m.match[5:end-1], ',')) for m in eachmatch(r"mul\((\d+),(\d+)\)", a)]
-p2 = sum([x[1] * x[2] for x in mul_vals])
