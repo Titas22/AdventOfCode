ee2c4058fb03b989aac5bb2de02333acae353ed4
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-05 23:06:02 +0000

    refactor: clean up 2024 day 5 solution

--- src/AoC_2024/AoC_2024_05.jl
+++ src/AoC_2024/AoC_2024_05.jl
@@ -1,105 +1,74 @@
 module AoC_2024_05
     using AdventOfCode;
+    using Parsers;
 
     function parse_inputs(lines::Vector{String})
 
-        return lines;
+        idx_empty = findfirst(x-> x == "", lines);
+        
+        str_deps = @view lines[1:idx_empty-1]
+        
+        str_lists = @view lines[idx_empty+1 : end]
+        lists = (x->parse.(Int, x)).(split.(str_lists, ','))
+        
+        deps = Dict();
+        for strdep in str_deps
+            idx_sep = findfirst('|', strdep)
+            v = Parsers.parse(Int64, strdep[1 : idx_sep-1])
+            k = Parsers.parse(Int64, strdep[idx_sep+1 : end])
+            if haskey(deps, k)
+                push!(deps[k], v)
+            else
+                deps[k] = Set(v);
+            end
+        end
+
+        return (deps, lists);
     end
-    function solve_common(inputs)
+    function solve_common(deps, lists)
+        bOk = falses(length(lists))
+        for (jj, l) in pairs(lists)
+            bOk[jj] = true
+            for ii in eachindex(l)
+                if haskey(deps, l[ii]) && findfirst(x -> x in deps[l[ii]], l[ii+1:end]) !== nothing
+                    bOk[jj] = false
+                    break;
+                end
+            end
+            bOk[jj] || continue;
+        end
 
-        return inputs;
+        return bOk;
     end
 
-    function solve_part_1(inputs)
-
-        return nothing;
+    function solve_part_1(lists)
+        tot = 0
+        for l in lists
+            tot += l[length(l)÷2 + 1]
+        end
+        return tot;
     end
 
-    function solve_part_2(inputs)
-
-        return nothing;
+    function solve_part_2(bad_lists, deps)
+        for l in bad_lists
+            sort!(l; lt=(x, y) -> haskey(deps, x) && y in deps[x])        
+        end
+        return solve_part_1(bad_lists);
     end
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
         lines       = @getinputs(btest);
-        # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
-        inputs      = parse_inputs(lines);
+        (deps, lists)      = parse_inputs(lines);
 
-        solution    = solve_common(inputs);
-        part1       = solve_part_1(solution);
-        part2       = solve_part_2(solution);
+        bOk         = solve_common(deps, lists);
+        part1       = solve_part_1(@view lists[bOk]);
+        part2       = solve_part_2(lists[.!bOk], deps);
 
         return (part1, part2);
     end
 
-    @time (part1, part2) = solve(true); # Test
-    # @time (part1, part2) = solve();
+    # @time (part1, part2) = solve(true); # Test
+    @time (part1, part2) = solve();
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(false)
-
-idx_empty = findfirst(x-> x == "", lines);
-
-str_deps = @view lines[1:idx_empty-1]
-
-str_lists = @view lines[idx_empty+1 : end]
-lists = (x->parse.(Int, x)).(split.(str_lists, ','))
-
-deps = Dict();
-for strdep in str_deps
-    idx_sep = findfirst('|', strdep)
-    v = Parsers.parse(Int64, strdep[1 : idx_sep-1])
-    k = Parsers.parse(Int64, strdep[idx_sep+1 : end])
-    if haskey(deps, k)
-        push!(deps[k], v)
-    else
-        deps[k] = Set(v);
-    end
-end
-deps
-
-tot = 0
-bOk = falses(length(lists))
-for (jj, l) in pairs(lists)
-    bOk[jj] = true
-    for ii in eachindex(l)
-        if haskey(deps, l[ii]) && findfirst(x -> x in deps[l[ii]], l[ii+1:end]) !== nothing
-            # l[ii+1:end] deps[l[ii]] in 
-            bOk[jj] = false
-
-            # @printf("Current index %d: value %d requires %s\n", ii, l[ii], string(deps[l[ii]]))
-            # println(l)
-            # println(l[1:ii-1])
-            break;
-        end
-    end
-    bOk[jj] || continue;
-    # println(l)
-    # println(l[length(l)÷2 + 1])
-end
-
-tot = 0
-for l in @view lists[bOk]
-    global tot
-    tot += l[length(l)÷2 + 1]
-end
-
-println(tot)
-
-bad_lists = @view lists[.!bOk]
-
-for l in bad_lists
-    @printf("Pre-sort: %s\n", string(l))
-    sort!(l; lt=(x, y) -> haskey(deps, x) && y in deps[x])
-    @printf("Post-sort: %s\n", string(l))
-
-end
-
-tot = 0
-for l in bad_lists
-    global tot
-    tot += l[length(l)÷2 + 1]
-end
-
-println(tot)
