34d483fb900b3501003233e8c0e301a6b1610cd4
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-06 01:30:56 +0000

    perf: improve 2024 day 5 solution

--- src/AoC_2024/AoC_2024_05.jl
+++ src/AoC_2024/AoC_2024_05.jl
@@ -2,67 +2,43 @@ module AoC_2024_05
     using AdventOfCode;
     using Parsers;
 
-    function parse_inputs(lines::Vector{String})
+    const deps = Dict{Int64, Set{Int64}}();
 
+    function parse_inputs(lines::Vector{String})
         idx_empty = findfirst(x-> x == "", lines);
         
         str_deps = @view lines[1:idx_empty-1]
-        
-        str_lists = @view lines[idx_empty+1 : end]
-        lists = (x->parse.(Int, x)).(split.(str_lists, ','))
-        
-        deps = Dict();
         for strdep in str_deps
-            idx_sep = findfirst('|', strdep)
-            v = Parsers.parse(Int64, strdep[1 : idx_sep-1])
-            k = Parsers.parse(Int64, strdep[idx_sep+1 : end])
-            if haskey(deps, k)
-                push!(deps[k], v)
-            else
-                deps[k] = Set(v);
-            end
+            (v,k) = Parsers.parse.(Int64, (strdep[1:2], strdep[4:5]))
+            haskey(deps, k) ? push!(deps[k], v) : push!(deps, k => Set(v));
         end
 
-        return (deps, lists);
+        str_updates = @view lines[idx_empty+1 : end]
+        updates     = (x->Parsers.parse.(Int, x)).(split.(str_updates, ','))
+        return updates;
     end
-    function solve_common(deps, lists)
-        bOk = falses(length(lists))
-        for (jj, l) in pairs(lists)
-            bOk[jj] = true
-            for ii in eachindex(l)
-                if haskey(deps, l[ii]) && findfirst(x -> x in deps[l[ii]], l[ii+1:end]) !== nothing
-                    bOk[jj] = false
-                    break;
-                end
-            end
-            bOk[jj] || continue;
-        end
+    
+    is_page_ordered(x::Int, y::Int)::Bool = haskey(deps, x) && y in deps[x];
 
-        return bOk;
-    end
+    solve_common(lists::Vector{Vector{Int}}) = issorted.(lists; lt=is_page_ordered, rev=true);
 
-    function solve_part_1(lists)
-        tot = 0
-        for l in lists
-            tot += l[length(l)÷2 + 1]
-        end
-        return tot;
-    end
+    get_middle_element(l::Vector{Int})::Int = l[(length(l)+1)÷2];
 
-    function solve_part_2(bad_lists, deps)
-        for l in bad_lists
-            sort!(l; lt=(x, y) -> haskey(deps, x) && y in deps[x])        
-        end
+    solve_part_1(lists::AbstractVector{Vector{Int}})::Int = mapreduce(get_middle_element, +, lists);
+
+    function solve_part_2!(bad_lists::AbstractVector{Vector{Int}})::Int
+        sort!.(bad_lists; lt=is_page_ordered, alg=InsertionSort)  
         return solve_part_1(bad_lists);
     end
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
-        lines       = @getinputs(btest);
-        (deps, lists)      = parse_inputs(lines);
+        lines   = @getinputs(btest);
+        updates = parse_inputs(lines);
 
-        bOk         = solve_common(deps, lists);
-        part1       = solve_part_1(@view lists[bOk]);
-        part2       = solve_part_2(lists[.!bOk], deps);
+        bOk     = solve_common(updates);
+        
+        part1   = solve_part_1(updates[bOk]);
+        part2   = solve_part_2!(updates[.!bOk]);
 
         return (part1, part2);
     end
