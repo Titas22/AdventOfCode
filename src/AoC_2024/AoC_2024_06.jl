947b9c89299659ce81befa6cf808a1d78a3164be
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-07 00:12:47 +0000

    feat: dirty 2024 day 6 part 2 solution

--- src/AoC_2024/AoC_2024_06.jl
+++ src/AoC_2024/AoC_2024_06.jl
@@ -6,7 +6,7 @@ module AoC_2024_06
     function parse_inputs(lines::Vector{String})::Tuple{Matrix{Char}, CartesianIndex{2}}
         charmat = lines2charmat(lines)
         idx_start::CartesianIndex{2} = findfirst(charmat .== '^')
-        # charmat[idx_start] = '.';
+        charmat[idx_start] = '.';
         return (charmat, idx_start);
     end
 
@@ -46,27 +46,50 @@ lines = @getinputs(false)
 
 
 (charmat, idx_start)    = AoC_2024_06.parse_inputs(lines);
+
+# directions = Queue{CartesianIndex{2}}(4)
+# enqueue!(directions, CartesianIndex(-1,0))
+# enqueue!(directions, CartesianIndex(0,1))
+# enqueue!(directions, CartesianIndex(1,0))
+# enqueue!(directions, CartesianIndex(0,-1))
+
+directions = (
+    CartesianIndex(-1,0), 
+    CartesianIndex(0,1), 
+    CartesianIndex(1,0), 
+    CartesianIndex(0,-1)
+    )
+
+
 visited = falses(size(charmat))
+# function init_directional_visited()
+# end
+sz = size(charmat)
+visdir = (falses(sz), falses(sz), falses(sz), falses(sz))
+#visdir = (copy(visited), copy(visited), copy(visited), copy(visited))
 
-visited[idx_start] = true;
+# visdir[1][idx_start] = true;
+# visited[idx_start] = true;
+# visdict = Dict{CartesianIndex{2}, BitMatrix}();
+# for ii = 1 : 4
+#     dir = dequeue!(directions)
+#     visdict[dir] = copy(visited);
+#     enqueue!(directions, dir)
+# end
 
-directions = Queue{CartesianIndex{2}}(4)
-enqueue!(directions, CartesianIndex(-1,0))
-enqueue!(directions, CartesianIndex(0,1))
-enqueue!(directions, CartesianIndex(1,0))
-enqueue!(directions, CartesianIndex(0,-1))
 
-function test!(visited, charmat, directions, pos)
-    dir = dequeue!(directions)
+function test(charmat, directions, pos)
+    visited = falses(size(charmat))
+    idx_dir = 1;
+    dir = directions[idx_dir];
     while true
         pos += dir
         checkbounds(Bool, visited, pos) || break;
 
         if charmat[pos] == '#'
             pos -= dir
-            # Turn and step back
-            enqueue!(directions, dir)
-            dir = dequeue!(directions)
+            idx_dir = mod(idx_dir, 4) + 1
+            dir = directions[idx_dir]
             pos -= dir
         else
             visited[pos] = true
@@ -75,6 +98,44 @@ function test!(visited, charmat, directions, pos)
     return visited;
 end
 
-test!(visited, charmat, directions, idx_start)
+@time visited = test(charmat, directions, idx_start)
+@time visited = test(charmat, directions, idx_start)
 
-count(visited)
+println(count(visited) + 1)
+
+
+function isloop(charmat, directions, pos)
+    visdir = (falses(sz), falses(sz), falses(sz), falses(sz))
+    idx_dir = 1;
+    visdir[idx_dir][pos] = true;
+
+    dir = directions[idx_dir];
+    while true
+        pos += dir
+        checkbounds(Bool, charmat, pos) || break;
+        visdir[idx_dir][pos] && return true;
+        if charmat[pos] == '#'
+            pos -= dir
+            idx_dir = mod(idx_dir, 4) + 1
+            dir = directions[idx_dir]
+            pos -= dir
+        else
+            visdir[idx_dir][pos] = true
+        end
+    end
+    return false;
+
+
+end
+
+nloops = 0
+org_path = findall(visited)
+for block in org_path
+    charmat[block] = '#'
+    if isloop(charmat, directions, idx_start) 
+        global nloops
+        nloops += 1;
+    end
+    charmat[block] = '.'
+end
+nloops
