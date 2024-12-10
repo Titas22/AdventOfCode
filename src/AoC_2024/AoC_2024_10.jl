e65b6651ece8edb350cc468fb4ff288e0b8a9802
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-10 23:35:12 +0000

    refactor: clean up 2024 day 10 solutions

--- src/AoC_2024/AoC_2024_10.jl
+++ src/AoC_2024/AoC_2024_10.jl
@@ -1,77 +1,55 @@
 module AoC_2024_10
     using AdventOfCode;
+    using DataStructures;
 
-    function parse_inputs(lines::Vector{String})
+    parse_inputs(lines::Vector{String})::Matrix{Int} = lines2charmat(lines) .- '0';
+    
+    const directions::Vector{CartesianIndex{2}} = CartesianIndex.([(-1,0), (1,0), (0,-1), (0,1)]);
 
-        return lines;
-    end
-    function solve_common(inputs)
+    function solve_common(heights::Matrix{Int})::Tuple{Int, Int}
+        scores1 = scores2 = 0;
 
-        return inputs;
-    end
+        for start_point in findall(x->x==0, heights)
+            searchList = Queue{Tuple{CartesianIndex{2}, Int}}();
+            enqueue!(searchList, (start_point, 0));
+        
+            peaks = CartesianIndex{2}[];
+        
+            while !isempty(searchList)
+                (pos, height)          = dequeue!(searchList);
+                if height == 9
+                    push!(peaks, pos)
+                    continue;
+                end
+                
+                for dir in directions
+                    next = pos + dir;
+                    checkbounds(Bool, heights, next) || continue;
+        
+                    heights[next] - height == 1 || continue;
+        
+                    enqueue!(searchList, (next, heights[next]));
+                end
+            end
+            
+            scores1 += length(unique(peaks));
+            scores2 += length(peaks);
+        end
 
-    function solve_part_1(inputs)
-
-        return nothing;
-    end
-
-    function solve_part_2(inputs)
-
-        return nothing;
+        return (scores1, scores2);
     end
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
         lines       = @getinputs(btest);
-        # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
-        inputs      = parse_inputs(lines);
+        heights     = parse_inputs(lines);
 
-        solution    = solve_common(inputs);
-        part1       = solve_part_1(solution);
-        part2       = solve_part_2(solution);
+        (part1, part2) = solve_common(heights);
 
         return (part1, part2);
     end
 
-    @time (part1, part2) = solve(true); # Test
-    # @time (part1, part2) = solve();
+    # @time (part1, part2) = solve(true); # Test
+    @time (part1, part2) = solve();
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(false)
-
-heights = lines2charmat(lines) .- '0';
-
-start_points = findall(x->x==0, heights)
-
-directions = CartesianIndex.([(-1,0), (1,0), (0,-1), (0,1)]);
-
-scores1 = 0
-scores2 = 0
-for start_point in start_points
-    searchList          = Queue{Tuple{CartesianIndex{2}, Int}}();
-    enqueue!(searchList, (start_point, 0));
-
-    peaks = CartesianIndex{2}[];
-
-    while !isempty(searchList)
-        (pos, height)          = dequeue!(searchList);
-        if height == 9
-            push!(peaks, pos)
-            continue;
-        end
-        
-        for dir in directions
-            next = pos + dir;
-            checkbounds(Bool, heights, next) || continue;
-
-            heights[next] - height == 1 || continue;
-
-            enqueue!(searchList, (next, heights[next]));
-        end
-    end
-    global scores1, scores2
-    scores1 += length(unique(peaks));
-    scores2 += length(peaks);
-end
-println(scores1)
-println(scores2)
