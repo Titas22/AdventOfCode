fcb62022d931126dcef0dadc2b2175a6f7e96d1b
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-15 16:49:43 +0000

    feat: dirty 2024 day 13 part 1 solution

--- src/AoC_2024/AoC_2024_13.jl
+++ src/AoC_2024/AoC_2024_13.jl
@@ -1,10 +1,23 @@
 module AoC_2024_13
     using AdventOfCode;
+    using Parsers;
 
-    function parse_inputs(lines::Vector{String})
-
-        return lines;
+    struct ClawMachine
+        ButtonA::CartesianIndex{2}
+        ButtonB::CartesianIndex{2}
+        Prize::CartesianIndex{2}
     end
+
+    function ClawMachine(three_lines::Vector{<:AbstractString})::ClawMachine
+        m = match.(r"X[\+\=](\d+), Y[\+\=](\d+)", three_lines)
+        btnA  = CartesianIndex(Parsers.parse(Int, m[1][1]), Parsers.parse(Int, m[1][2]))
+        btnB  = CartesianIndex(Parsers.parse(Int, m[2][1]), Parsers.parse(Int, m[2][2]))
+        prize = CartesianIndex(Parsers.parse(Int, m[3][1]), Parsers.parse(Int, m[3][2]))
+        return ClawMachine(btnA, btnB, prize)
+    end
+
+    parse_inputs(lines::Vector{String})::Vector{ClawMachine} = ClawMachine.(split_at_empty_lines(lines))
+
     function solve_common(inputs)
 
         return inputs;
@@ -37,4 +50,23 @@ module AoC_2024_13
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(true)
+lines = @getinputs(false)
+
+machines = AoC_2024_13.parse_inputs(lines)
+
+function find_cheapest_win(machine::AoC_2024_13.ClawMachine)::Int
+    min_cost = 1000
+    for iA = 0 : 100
+        for iB = 0 : 100
+            if machine.ButtonA * iA + machine.ButtonB * iB == machine.Prize  
+                cost = iA * 3 + iB;
+                cost < min_cost || continuel
+                min_cost = cost;
+            end
+        end
+    end
+    return min_cost
+end
+
+costs = find_cheapest_win.(machines)
+mapreduce(x -> x == 1000 ? 0 : x, +, costs)
