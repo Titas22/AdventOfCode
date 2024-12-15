e98ba490469aa5bf6fa3b8419ccd3df6ecead2c0
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-15 02:03:30 +0000

    feat: dirty 2024 day 14 part 1 solution

--- src/AoC_2024/AoC_2024_14.jl
+++ src/AoC_2024/AoC_2024_14.jl
@@ -1,9 +1,36 @@
 module AoC_2024_14
     using AdventOfCode;
+    using Parsers;
 
-    function parse_inputs(lines::Vector{String})
+    struct Robot
+        position::Vector{Int}
+        velocity::Tuple{Int, Int}
+    end
+    
+    function split_values(str::AbstractString)::Tuple{Int, Int}
+        idx = findfirst(x->x==',', str)
+        return (Parsers.parse(Int, str[idx+1:end]), Parsers.parse(Int, str[1:idx-1]))
+    end
+    
+    function Robot(line::AbstractString)::Robot
+        idx = findfirst(x->x==' ', line)
+        pos = split_values(line[3 : idx-1])
+        vel = split_values(line[idx+3 : end])
+    
+        return Robot(collect(pos), vel)
+    end
 
-        return lines;
+    function move!(robot::Robot, t::Int, sz::Tuple{Int, Int})
+        new_pos = robot.position .+ robot.velocity .* t
+        robot.position[1] = mod(new_pos[1], sz[1])
+        robot.position[2] = mod(new_pos[2], sz[2])
+    end
+
+    parse_inputs(lines::Vector{String})::Vector{Robot} = Robot.(lines);
+
+    function calculate_safety_factor(robots::Vector{Robot}, sz::Tuple{Int, Int})::Int
+        mid = sz .÷ 2
+
     end
     function solve_common(inputs)
 
@@ -37,4 +64,28 @@ module AoC_2024_14
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(true)
+bTest = false
+lines = @getinputs(bTest)
+
+robots = AoC_2024_14.parse_inputs(lines)
+
+sz = bTest ? (7, 11) : (103, 101)
+
+t = 100
+
+for robot in robots
+    AoC_2024_14.move!(robot, t, sz)
+end
+
+# AoC_2024_14.calculate_safety_factor(robots)
+
+qcounts = zeros(Int, 4, 1)
+mid = sz .÷ 2
+for robot in robots
+    (robot.position[1] == mid[1] || robot.position[2] == mid[2]) && continue
+    idx_quadrant = (robot.position[2] < mid[2] ? 1 : 2) + (robot.position[1] < mid[1] ? 0 : 2)    
+    qcounts[idx_quadrant] += 1
+end
+qcounts
+
+prod(qcounts)
