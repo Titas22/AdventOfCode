cb0682742375308a14364f6f5ec7659d0d437f50
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-15 20:52:25 +0000

    refactor: clean up 2024 day 12 solution

--- src/AoC_2024/AoC_2024_12.jl
+++ src/AoC_2024/AoC_2024_12.jl
@@ -1,6 +1,7 @@
 module AoC_2024_12
     using AdventOfCode;
 
+    const directions::Vector{CartesianIndex{2}} = CartesianIndex.([(-1,0), (0,-1), (1,0), (0,1)]);
     struct Region
         Symbol::Char
         Area::Int
@@ -8,112 +9,75 @@ module AoC_2024_12
         Sides::Int
     end
 
-    function parse_inputs(lines::Vector{String})
-
-        return lines;
-    end
-    function solve_common(inputs)
-
-        return inputs;
+    parse_inputs(lines::Vector{String})::Matrix{Char} = lines2charmat(lines)
+    
+    function is_same_plant_type(chmat::Matrix{Char}, pos::CartesianIndex{2}, symbol::Char)::Bool
+        return checkbounds(Bool, chmat, pos) && chmat[pos] == symbol
     end
 
-    function solve_part_1(inputs)
+    function solve_common(chmat::Matrix{Char})::Vector{Region}
+        notprocessed = trues(size(chmat))
 
-        return nothing;
+        searchList = CartesianIndex{2}[];
+        regions = Region[];
+        
+        for idx in CartesianIndices(chmat)
+            notprocessed[idx] || continue
+            push!(searchList, idx);
+        
+            symbol = chmat[idx]
+            area = perimeter = corners = 0;
+            while !isempty(searchList)
+                pos = popfirst!(searchList);
+                notprocessed[pos] || continue
+                notprocessed[pos] = false
+                area += 1
+        
+                for (ii, dir) in pairs(directions)
+                    next = pos + dir;
+                    ccw_pos = pos + directions[mod(ii, 4)+1];
+                    diag_pos = ccw_pos + dir 
+        
+                    isnext  = is_same_plant_type(chmat, next, symbol);
+                    isadj   = is_same_plant_type(chmat, ccw_pos, symbol)
+                    isdiag  = is_same_plant_type(chmat, diag_pos, symbol)
+        
+                    if !isnext
+                        perimeter += 1
+                        if !isadj #&& !isdiag
+                            corners += 1
+                        end
+                        continue
+                    else
+                        if !isadj && isdiag
+                            corners += 1
+                        end
+                    end
+        
+                    push!(searchList, next);
+                end
+            end
+            push!(regions, Region(symbol, area, perimeter, corners))
+        end
+        return regions;
     end
 
-    function solve_part_2(inputs)
-
-        return nothing;
-    end
+    solve_part_1(regions::Vector{Region})::Int = mapreduce(r -> r.Area * r.Perimeter, +, regions)
+    solve_part_2(regions::Vector{Region})::Int = mapreduce(r -> r.Area * r.Sides, +, regions)
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
         lines       = @getinputs(btest);
-        # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
-        inputs      = parse_inputs(lines);
+        charmat     = parse_inputs(lines);
 
-        solution    = solve_common(inputs);
-        part1       = solve_part_1(solution);
-        part2       = solve_part_2(solution);
+        regions     = solve_common(charmat);
+        part1       = solve_part_1(regions);
+        part2       = solve_part_2(regions);
 
         return (part1, part2);
     end
 
-    @time (part1, part2) = solve(true); # Test
-    # @time (part1, part2) = solve();
+    # @time (part1, part2) = solve(true); # Test
+    @time (part1, part2) = solve();
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(false)
-
-chmat = lines2charmat(lines)
-
-const directions::Vector{CartesianIndex{2}} = CartesianIndex.([(-1,0), (0,-1), (1,0), (0,1)]);
-
-notprocessed = trues(size(chmat))
-
-searchList = CartesianIndex{2}[];
-regions = AoC_2024_12.Region[];
-
-function is_same_plant_type(chmat::Matrix{Char}, pos::CartesianIndex{2}, symbol::Char)::Bool
-    return checkbounds(Bool, chmat, pos) && chmat[pos] == symbol
-end
-
-for idx in CartesianIndices(chmat)
-    notprocessed[idx] || continue
-    push!(searchList, idx);
-
-    symbol = chmat[idx]
-    area = perimeter = corners = 0;
-    global coords
-    coords = CartesianIndex{2}[]
-    while !isempty(searchList)
-        pos = popfirst!(searchList);
-        notprocessed[pos] || continue
-        notprocessed[pos] = false
-        push!(coords, pos)
-        area += 1
-
-        for (ii, dir) in pairs(directions)
-            next = pos + dir;
-            ccw_pos = pos + directions[mod(ii, 4)+1];
-            diag_pos = ccw_pos + dir 
-
-            isnext  = is_same_plant_type(chmat, next, symbol);
-            isadj   = is_same_plant_type(chmat, ccw_pos, symbol)
-            isdiag  = is_same_plant_type(chmat, diag_pos, symbol)
-
-            if !isnext
-                perimeter += 1
-                if !isadj #&& !isdiag
-                    corners += 1
-                end
-                continue
-            else
-                if !isadj && isdiag
-                    corners += 1
-                end
-            end
-
-            push!(searchList, next);
-        end
-    end
-    push!(regions, AoC_2024_12.Region(symbol, area, perimeter, corners))
-end
-
-
-# plant_types = unique(chmat);
-
-display(regions)
-
-p1 = mapreduce(r -> r.Area * r.Perimeter, +, regions)
-p2 = mapreduce(r -> r.Area * r.Sides, +, regions)
-
-
-# for (ii, dir) in pairs(directions)
-#     jj = mod(ii, 4)+1
-#     @printf("%d->%d dir: %s   cw_dir: %s\n", ii, jj, dir, directions[jj])
-# end
-
-# # 865044 - too low
-# 872382
