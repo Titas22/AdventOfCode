d7d28ddcef66d22a1621e03fd5488bf5436aeb15
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-15 22:28:04 +0000

    feat: dirty 2024 day 15 part 1 solution

--- src/AoC_2024/AoC_2024_15.jl
+++ src/AoC_2024/AoC_2024_15.jl
@@ -23,7 +23,6 @@ module AoC_2024_15
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
         lines       = @getinputs(btest);
-        # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
         inputs      = parse_inputs(lines);
 
         solution    = solve_common(inputs);
@@ -38,4 +37,72 @@ module AoC_2024_15
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(true)
+lines = @getinputs(true, "_simple")
+# lines = @getinputs(true)
+
+inputs = split_at_empty_lines(lines)
+map = lines2charmat(inputs[1])
+moves = join(inputs[2])
+
+robot_pos = findfirst(x -> x=='@', map)
+map[robot_pos] = '.'
+
+is_end(ch::Char)::Bool = ch == '#' || ch == '.'
+
+function showmap(map::Matrix{Char}, robot_pos::CartesianIndex{2})
+    copymap = copy(map);
+    copymap[robot_pos] = '@'
+    display(copymap)
+end
+
+showmap(map, robot_pos)
+
+for move in moves
+    global robot_pos, map
+    if move == '^'
+        dir = CartesianIndex(-1, 0)
+    elseif move == '<'
+        dir = CartesianIndex(0, -1)
+    elseif move == '>'
+        dir = CartesianIndex(0, 1)
+    else # move == 'v'
+        dir = CartesianIndex(1, 0)
+    end
+
+    next = robot_pos + dir
+
+    ii = 0
+    while !is_end(map[next])
+        ii += 1
+        next += dir
+    end
+
+    if map[next] == '#'
+        # println("Move: " * string(move))
+        # showmap(map, robot_pos)
+        continue 
+    end
+    
+    for jj = ii : -1 : 1
+        map[next] = 'O'
+        next -= dir
+        map[next] = '.'
+    end
+
+    # println("Move: " * string(move))
+    # showmap(map, next)
+    robot_pos = next
+end
+showmap(map, robot_pos)
+
+gps_sum = 0
+
+gps_coordinate(idx::CartesianIndex{2})::Int = (idx[1]-1) * 100 + idx[2] - 1
+
+for idx in CartesianIndices(map)
+    map[idx] == 'O' || continue
+    println(idx)
+    println(gps_coordinate(idx))
+    global gps_sum
+    gps_sum += gps_coordinate(idx)
+end
