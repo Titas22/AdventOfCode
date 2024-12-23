ea914e219a8eba556bbd5fa1484298e6fb92c198
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-23 02:38:42 +0200

    perf: improve 2024 day 17 solution

--- src/AoC_2024/AoC_2024_17.jl
+++ src/AoC_2024/AoC_2024_17.jl
@@ -23,32 +23,9 @@ module AoC_2024_17
         throw(BoundsError(operand, "reserved operand - should not appear in valid program"))
     end
 
-    dv(c::Computer, operand::Int)::Int = c.A ÷ 2^combo(c, operand)
+    dv(c::Computer, operand::Int)::Int = c.A >> combo(c, operand) # = c.A ÷ 2^combo(c, operand)
 
-    combomod8(c::Computer, operand::Int)::Int = mod(combo(c, operand), 8)
-
-    # Opcode 0
-    function adv!(c::Computer, operand::Int)
-        c.A = dv(c, operand)
-    end
-    # Opcode 6
-    function bdv!(c::Computer, operand::Int)
-        c.B = dv(c, operand)
-    end
-    # Opcode 7
-    function cdv!(c::Computer, operand::Int)
-        c.C = dv(c, operand)
-    end
-
-    # Opcode 1
-    function bxl(c::Computer, operand::Int)
-        c.B = c.B ⊻ operand
-    end
-
-    # Opcode 2
-    function bst!(c::Computer, operand::Int)
-        c.B = combomod8(c, operand)
-    end
+    combomod8(c::Computer, operand::Int)::Int = combo(c, operand) & 7 # = mod(combo(c, operand), 8)
 
     # Opcode 3
     function jnz!(c::Computer, operand::Int)
@@ -56,22 +33,26 @@ module AoC_2024_17
         c.pointer = operand - 2;
     end
 
-    # Opcode 4
-    function bxc!(c::Computer, operand::Int)
-        c.B = c.C ⊻ c.B
-    end
-
-    # Opcode 5
-    function out!(c::Computer, operand::Int)
-        push!(c.output, combomod8(c, operand))
-    end
-
-    const opcodes::NTuple{8, Function} = (adv!, bxl, bst!, jnz!, bxc!, out!, bdv!, cdv!)
-
-    function do_opcode!(c::Computer)
-        opcode = c.program[c.pointer+1]
-        operand = c.program[c.pointer+2]
-        opcodes[opcode+1](c, operand)
+    @inline function do_opcode!(c::Computer)
+        @inbounds opcode = c.program[c.pointer+1]
+        @inbounds operand = c.program[c.pointer+2]
+        if opcode == 0
+            setproperty!(c, :A, dv(c, operand)) # adv
+        elseif opcode == 1 
+            setproperty!(c, :B, c.B ⊻ operand) # bxl
+        elseif opcode == 2
+            setproperty!(c, :B, combomod8(c, operand)) # bst
+        elseif opcode == 3
+            jnz!(c, operand) # jnz
+        elseif opcode == 4
+            setproperty!(c, :B, c.C ⊻ c.B) # bxc
+        elseif opcode == 5
+            push!(c.output, combomod8(c, operand)) # out
+        elseif opcode == 6
+            setproperty!(c, :B, dv(c, operand)) # bdv
+        else #if opcode == 7 
+            setproperty!(c, :C, dv(c, operand)) # cdv
+        end
         c.pointer += 2
     end
 
@@ -94,28 +75,25 @@ module AoC_2024_17
         regB = parse_registry(lines[2])
         regC = parse_registry(lines[3])
         prog = Parsers.parse.(Int, split(lines[5][10:end], ','))
-        
         return Computer(regA, regB, regC, prog)
     end
 
     solve_part_1(c::Computer)::String = join(run!(c), ',')
 
     function solve_part_2(c::Computer)::Int
-        q::Queue{Int} = Queue{Int}()
-        enqueue!(q, 0)
+        q::Vector{Int} = [0]
         while !isempty(q)
-            prev = dequeue!(q)
-            
+            prev = popfirst!(q)
             for ii in 0 : 7
-                newA = prev*8 + ii
+                newA = prev * 8 + ii
                 c.A = newA
 
                 c.program == run!(c) && return newA
 
-                n = length(c.output) - 1
-                c.program[end-n : end] == c.output || continue
+                n = length(c.output)
+                @inbounds all(c.program[end-n+ii] == c.output[ii] for ii in 1:n) || continue
 
-                enqueue!(q, newA)
+                push!(q, newA)
                 # println("A = " * string(newA) * "(" * bitstring(newA) * ") -> " * string(c.output))
             end
         end
@@ -124,7 +102,6 @@ module AoC_2024_17
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
         lines       = @getinputs(btest);
-        # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
         computer    = parse_inputs(lines);
 
         part1       = solve_part_1(computer);
@@ -138,5 +115,3 @@ module AoC_2024_17
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-# 6,5,4,7,1,6,0,3,1
-# 106086382266778
