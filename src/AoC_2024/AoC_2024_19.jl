5efd9df2734d53bbe5190cafccae53b5ad22de5c
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-24 01:07:10 +0200

    refactor: clean up 2024 day 19 solution

--- src/AoC_2024/AoC_2024_19.jl
+++ src/AoC_2024/AoC_2024_19.jl
@@ -8,60 +8,53 @@ module AoC_2024_19
         return (patterns, designs);
     end
 
-    function solve_part_1(patterns, design)
-
-        return nothing;
+    function ispossible!(cache::Dict{<:AbstractString, Int}, design::AbstractString, patterns::Vector{<:AbstractString})::Int
+        haskey(cache, design) && return cache[design]
+        count = 0
+        for pattern in patterns
+            np = length(pattern)
+            np > length(design) && continue
+            if np == length(design)
+                if design == pattern 
+                    count += 1
+                end
+                continue
+            end
+    
+            startswith(design, pattern) || continue
+            count += ispossible!(cache, design[np+1 : end], patterns)
+        end
+        cache[design] = count
+        return count
     end
 
-    function solve_part_2(patterns, design)
+    function solve_common(patterns, designs)::Tuple{Int, Int}
+        cache = Dict{AbstractString, Int}()
 
-        return nothing;
+        possible_designs = 0
+        ncombinations = 0
+
+        for design in designs
+            ncombs = ispossible!(cache, design, patterns)
+            ncombs == 0 && continue
+            possible_designs += 1
+            ncombinations += ncombs
+        end
+
+        return (possible_designs, ncombinations)
     end
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
-        lines       = @getinputs(btest);
-        # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
-        (patterns, design)      = parse_inputs(lines);
+        lines               = @getinputs(btest);
+        (patterns, design)  = parse_inputs(lines);
 
-        part1       = solve_part_1(patterns, design);
-        part2       = solve_part_2(patterns, design);
+        (part1, part2)      = solve_common(patterns, design)
 
         return (part1, part2);
     end
 
-    @time (part1, part2) = solve(true); # Test
-    # @time (part1, part2) = solve();
+    # @time (part1, part2) = solve(true); # Test
+    @time (part1, part2) = solve();
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
 end
-lines = @getinputs(false)
-(patterns, designs) = AoC_2024_19.parse_inputs(lines)
-
-cache = Dict{AbstractString, Int}()
-
-function ispossible!(cache::Dict{<:AbstractString, Int}, design::AbstractString, patterns::Vector{<:AbstractString})::Int
-    haskey(cache, design) && return cache[design]
-    count = 0
-    for pattern in patterns
-        np = length(pattern)
-        np > length(design) && continue
-        if np == length(design)
-            if design == pattern 
-                count += 1
-            end
-            continue
-        end
-
-        startswith(design, pattern) || continue
-        count += ispossible!(cache, design[np+1 : end], patterns)
-    end
-    cache[design] = count
-    return count
-end
-
-ispossible(design::AbstractString)::Int = ispossible!(cache, design, patterns)
-
-@time possibilities = ispossible.(designs)
-
-part1 = count(possibilities .> 0 )
-part2 = sum(possibilities)
