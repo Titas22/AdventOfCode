1b04d5b55f27899110a4dfd384fecdb295e4dd98
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-30 20:35:23 +0200

    perf: improve 2024 day 22 solution

--- src/AoC_2024/AoC_2024_22.jl
+++ src/AoC_2024/AoC_2024_22.jl
@@ -1,6 +1,7 @@
 module AoC_2024_22
     using AdventOfCode
     using Parsers
+    using StaticArrays
 
     parse_inputs(lines::Vector{String})::Vector{Int} = Parsers.parse.(Int, lines)
 
@@ -15,34 +16,44 @@ module AoC_2024_22
         return secret
     end
 
+    @inline function update_buffer!(vec::MVector{4, Int}, new_value::Int)
+        @inbounds for i in 1:3
+            vec[i] = vec[i + 1]
+        end
+        vec[4] = new_value
+    end
+
+    const MSIZE::Tuple{Int64,Int64,Int64,Int64} = (19, 19, 19, 19)
+    
+    @inline _sub2ind(vec::MVector{4, Int}) = vec[1] + (vec[2]-1)*19 + (vec[3]-1)*361 + (vec[4]-1)*6859
+
     function solve_common(initial_secrets::Vector{Int})::Tuple{Int, Int}
         nsales = 2000
         
-        total_gainz = zeros(Int, 19, 19, 19, 19)
+        total_gainz = zeros(Int, MSIZE)
         total_secrets = 0
+        already_sold = falses(MSIZE)
+        last_changes = MVector(0,0,0,0)
+
         for secret in initial_secrets
-            last_changes = zeros(Int, 4)
             last_price = mod(secret, 10)
-            already_sold = falses(19, 19, 19, 19)
-            for ii = 1 : nsales
+            fill!(already_sold, false)
+            for _ = 1 : nsales
                 secret = evolve(secret)
                 price = mod(secret, 10)
-        
-                popfirst!(last_changes)
-                push!(last_changes, price - last_price + 10)
+
+                update_buffer!(last_changes, price - last_price + 10)
                 last_price = price
         
-                ii > 3 || continue
-        
-                idx = CartesianIndex(last_changes[1], last_changes[2], last_changes[3], last_changes[4])
-                already_sold[idx] && continue
-                total_gainz[idx] += price
-                already_sold[idx] = true
+                lidx = _sub2ind(last_changes)
+                @inbounds already_sold[lidx] && continue
+                @inbounds total_gainz[lidx] += price
+                @inbounds already_sold[lidx] = true
             end
             total_secrets += secret
         end
         
-        (gainz, idx) = findmax(total_gainz)
+        (gainz, _) = findmax(total_gainz)
         return (total_secrets, gainz)
     end
 
@@ -59,7 +70,4 @@ module AoC_2024_22
     @time (part1, part2) = solve();
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
-
-    @assert(part1 == 16619522798, "Part 1 is wrong")
-    @assert(part2 == 1854, "Part 2 is wrong")
 end
