6664465b33202e269eeb8a52f440d61b6bffb3bb
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-30 17:26:15 +0200

    perf: improve and clean up 2024 day 21 solution

--- src/AoC_2024/AoC_2024_21.jl
+++ src/AoC_2024/AoC_2024_21.jl
@@ -20,9 +20,6 @@ module AoC_2024_21
     )
     get_direction_char(dir::CartesianIndex{2})::Char = DIRECTION_MAP[dir]
 
-    # const DIRECTION_MAP = SVector('^', 'v', '<', '>')
-    # get_direction_char(dir::CartesianIndex{2})::Char = DIRECTION_MAP[1 + (dir[1] + 1) + (dir[2] + 1) * 2]
-    
     is_row_with_empty(keypad::NumericKeypad, idx::CartesianIndex{2})::Bool = idx[1] == 4
     is_row_with_empty(keypad::DirectionalKeypad, idx::CartesianIndex{2})::Bool = idx[1] == 1
     is_col_with_empty(keypad::Keypad, idx::CartesianIndex{2})::Bool = idx[2] == 1
@@ -69,7 +66,7 @@ module AoC_2024_21
         return [vpath * hpath, hpath * vpath]
     end
 
-    function get_distance_map(keypad::Keypad)
+    function get_distance_map(keypad::Keypad)::Dict{Char, Dict{Char, Vector{String}}}
         dict = Dict{Char, Dict{Char, Vector{String}}}()
         for from = CartesianIndices(keypad)
             dict[keypad[from]] = Dict{Char, Vector{String}}()
@@ -79,7 +76,11 @@ module AoC_2024_21
                 delta = to - from
                 paths = get_paths(keypad, from, to)
                 # println("from: " * keypad[from] * "   to: " * keypad[to] * "   delta: " * string(paths))
-
+                
+                # We know each directional input will have to end with an 'A'
+                if isa(keypad, DirectionalKeypad)
+                    paths .*= 'A'
+                end
                 dict[keypad[from]][keypad[to]] = paths
             end
         end
@@ -87,55 +88,46 @@ module AoC_2024_21
         return dict
     end
     
-    numeric_distances = get_distance_map(NUMERIC_KEYPAD)
-    directional_distances = get_distance_map(DIRECTIONAL_KEYPAD)
+    numeric_distances::Dict{Char, Dict{Char, Vector{String}}} = get_distance_map(NUMERIC_KEYPAD)
+    directional_distances::Dict{Char, Dict{Char, Vector{String}}} = get_distance_map(DIRECTIONAL_KEYPAD)
 
-    # cache::Dict{UInt, BigInt} = Dict{UInt, BigInt}()
-    # hash(str::String, num::Int)::UInt = Base.hash(str * Char(num))
-    cache::Dict{Tuple{String, Int}, BigInt} = Dict{Tuple{String, Int}, BigInt}()
+    cache::Dict{Tuple{String, Int}, Int} = Dict{Tuple{String, Int}, Int}()
 
-    function find_input_sequence_length(directional_moves::String; depth::Int)::BigInt
-        # k = hash(directional_moves, depth)
+    function find_input_sequence_length(directional_moves::String; depth::Int)::Int
         k = (directional_moves, depth)
         haskey(cache, k) && return cache[k]
         input_sequence_length = 0
         robot_pos = 'A'
         
         for move in directional_moves
-            shortest_move = BigInt(0)
+            shortest_move = typemax(Int)
             for path in directional_distances[robot_pos][move]
                 if depth == 1
-                    path_length =  BigInt(length(path))+1
+                    path_length =  length(path)
                 else
-                    path_length =  find_input_sequence_length(path * 'A'; depth=depth-1)
+                    path_length =  find_input_sequence_length(path; depth=depth-1)
                 end
-                if shortest_move == 0 || path_length < shortest_move 
-                    shortest_move = path_length
-                end
+                path_length < shortest_move || continue
+                shortest_move = path_length
             end
             input_sequence_length += shortest_move
-            # path = directional_distances[robot_pos][move][1] * 'A'
-            # if depth == 1
-            #     input_sequence_length += length(path)
-            # else
-            #     input_sequence_length += find_input_sequence_length(path; depth=depth-1)
-            # end
+
             robot_pos = move
         end
         cache[k] = input_sequence_length
         return input_sequence_length
     end
     
-    function find_shortest_inputs(code::String, num_directional_robots::Int = 2)::BigInt
+    function find_shortest_inputs(code::String, num_directional_robots::Int = 2)::Int
         numeric_location = 'A'
-        total_shortest_input = BigInt(0)
+        total_shortest_input = 0
         for num in code
             numeric_paths = numeric_distances[numeric_location][num] .* 'A'
             
-            shortest_num_input = BigInt(0)
+            shortest_num_input = typemax(Int)
             for num_path in numeric_paths
                 input_length = find_input_sequence_length(num_path; depth=num_directional_robots)
-                input_length > shortest_num_input > 0 && continue
+                shortest_num_input > input_length || continue
                 shortest_num_input = input_length
             end
     
@@ -146,7 +138,7 @@ module AoC_2024_21
         return total_shortest_input
     end
 
-    function solve_common(lines::Vector{String}; depth::Int)::BigInt
+    function solve_common(lines::Vector{String}; depth::Int)::Int
         total = 0
         for line in lines
             shortest_path_length = find_shortest_inputs(line, depth)
@@ -161,7 +153,7 @@ module AoC_2024_21
     function solve(btest::Bool = false)::Tuple{Any, Any}
         lines       = @getinputs(btest);
 
-        empty!(cache)
+        empty!(cache) # Clean here to not cache between benchmarks
         part1       = solve_part_1(lines);
         part2       = solve_part_2(lines);
 
@@ -173,6 +165,6 @@ module AoC_2024_21
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
     
-    @assert(part1 == 128962, "Part 1 is wrong")
-    @assert(part2 == 159684145150108, "Part 2 is wrong")
+    # @assert(part1 == 128962, "Part 1 is wrong")
+    # @assert(part2 == 159684145150108, "Part 2 is wrong")
 end
