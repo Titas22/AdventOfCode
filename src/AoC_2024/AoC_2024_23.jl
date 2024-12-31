1a27f27bf84e05d1e19fc713958233fca16d146e
Titas22 <66130422+Titas22@users.noreply.github.com>
2024-12-31 18:39:00 +0200

    refactor: clean up 2024 day 23 solution and use Symbols for speedup

--- src/AoC_2024/AoC_2024_23.jl
+++ src/AoC_2024/AoC_2024_23.jl
@@ -1,20 +1,52 @@
 module AoC_2024_23
     using AdventOfCode
 
-    const Links = Dict{String, Set{String}}
+    const Links = Dict{Symbol, Set{Symbol}}
+
+    function add_link!(d::Links, a::Symbol, b::Symbol)
+        if !haskey(d, a)
+            d[a] = Set{Symbol}()
+        end
+        push!(d[a], b)
+    end
+    function add_tsymbols!(tsymbols::Set{Symbol}, a::String, sa::Symbol)
+        a[1] == 't' || return
+        push!(tsymbols, sa)
+    end
+    function add_links!(d::Links, tsymbols::Set{Symbol}, a::String, b::String)
+        sa = Symbol(a)
+        sb = Symbol(b)
+        add_link!(d, sa, sb)
+        add_link!(d, sb, sa)
+        add_tsymbols!(tsymbols, a, sa)
+        add_tsymbols!(tsymbols, b, sb)
+    end
 
     function parse_inputs(lines::Vector{String})
-
-        return lines;
+        links = Links()
+        tsymbols = Set{Symbol}()
+        for line in lines
+            add_links!(links, tsymbols, line[1:2], line[4:5])
+        end
+        return (links, tsymbols)
     end
-    function solve_common(inputs)
 
-        return inputs;
+    function find_trios!(all_trios::Set{Set{Symbol}}, links::Links, k::Symbol)
+        thirds = Set{Symbol}()
+        for k2 in links[k]
+            second = links[k2]
+            for k3 in second
+                k3 in links[k] && k3 ∉ thirds || continue
+                push!(all_trios, Set{Symbol}((k,k2,k3)))
+            end
+        end
     end
-
-    function solve_part_1(inputs)
-
-        return nothing;
+    function solve_part_1(links::Links, tsymbols::Set{Symbol})
+        all_trios = Set{Set{Symbol}}()
+        for k in tsymbols
+            find_trios!(all_trios, links, k)
+        end
+        return length(all_trios)
     end
 
     function solve_part_2(inputs)
@@ -23,57 +55,23 @@ module AoC_2024_23
     end
 
     function solve(btest::Bool = false)::Tuple{Any, Any}
-        lines       = @getinputs(btest);
-        # lines2      = @getinputs(btest, "_2"); # Use if 2nd problem test case inputs are different
-        inputs      = parse_inputs(lines);
+        lines       = @getinputs(btest)
+        (links, tsymbols) = parse_inputs(lines)
 
-        solution    = solve_common(inputs);
-        part1       = solve_part_1(solution);
-        part2       = solve_part_2(solution);
+        part1       = solve_part_1(links, tsymbols);
+        part2       = solve_part_2(links);
 
         return (part1, part2);
     end
 
-    @time (part1, part2) = solve(true); # Test
-    # @time (part1, part2) = solve();
+    # @time (part1, part2) = solve(true); # Test
+    @time (part1, part2) = solve();
     println("\nPart 1 answer: $(part1)");
     println("\nPart 2 answer: $(part2)\n");
-end
-lines = @getinputs(false)
 
-function add_link!(d::Dict{String, Set{String}}, a::String, b::String)
-    if !haskey(d, a)
-        d[a] = Set{String}()
-    end
-    push!(d[a], b)
+    # @assert(part1 == 1248, "Part 1 is wrong")
+    # @assert(part2 == , "Part 2 is wrong")
 end
-function add_links!(d::Dict{String, Set{String}}, a::String, b::String)
-    add_link!(d, a, b)
-    add_link!(d, b, a)
-end
+# lines = @getinputs(true)
 
 
-links = Dict{String, Set{String}}()
-
-for line in lines
-    add_links!(links, line[1:2], line[4:5])
-end
-
-function add_trios!(all_trios::Set{Set{String}}, links::AoC_2024_23.Links, k::String)
-    thirds = Set{String}()
-    for k2 in links[k]
-        second = links[k2]
-        for k3 in second
-            k3 in links[k] && k3 ∉ thirds || continue
-            # println("$k-$k2-$k3")
-            push!(all_trios, Set{String}((k,k2,k3)))
-        end
-    end
-end
-
-all_trios = Set{Set{String}}()
-for k in keys(links)
-    startswith(k, 't') || continue
-    add_trios!(all_trios, links, k)
-end
-length(all_trios)
